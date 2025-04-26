e476d633c58d739481f3f9f612873829ef6865a3
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-04-26 23:51:48 +1200

    Added addMines function which will add the class mines to around 20% based off the current difficulty, can change later on though to higher or lower rates depending on the users preferences

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let boardSize = 5;
+let difficulty = 0.2;
 
 function createBoard() {
   let HTML = ``
@@ -9,3 +10,16 @@ function createBoard() {
 }
 
 createBoard();
+
+function addMines() {
+  const max = boardSize * boardSize
+  const amountOfMines = max * difficulty;
+
+  for (let i = 0; i < amountOfMines; i++) {
+    let id = Math.floor(Math.random() * max)
+    document.getElementById(`cell-${id}`).classList.add('mine');
+  }
+  
+}
+
+addMines();
