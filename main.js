0a654df1f2cabbc3ade6615927dd78aa3d03cd0b
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-04-27 10:24:32 +1200

    Cells now generate with a row class for easier searching later on

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ let difficulty = 0.2;
 
 function createBoard() {
   let HTML = ``
-  for(let i = 0; i < boardSize * boardSize; i++) {
-    HTML += `<div id="cell-${i}" class="cell"></div>`
+  for(let i = 0; i < boardSize; i++) {
+    for(let j = 0; j < boardSize; j++)
+    HTML += `<div id="cell-${i}" class="cell row-${j}"></div>`
   }
   document.getElementById('game-container').innerHTML = HTML
 }
@@ -19,7 +20,7 @@ function addMines() {
     let id = Math.floor(Math.random() * max)
     document.getElementById(`cell-${id}`).classList.add('mine');
   }
-  
 }
 
 addMines();
+
