ad880765c738c8c5e73b7260b7939786eca5a5ab
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-04-28 14:30:26 +1200

    Added a function that checks all the squares around a square and returns the number of mines around it and attached that function to all the boxes where it puts the number of mines into the box of text, if that number is zero it will do this action to all of the boxes around it aswell

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 let boardSize = 5;
 let difficulty = 0.2;
+const grid = [];
 
 function createBoard() {
   let HTML = ``
-  for(let i = 0; i < boardSize; i++) {
-    for(let j = 0; j < boardSize; j++)
-    HTML += `<div id="cell-${i}" class="cell row-${j}"></div>`
+  for(let row = 0; row < boardSize; row++) {
+    grid[row] = [];
+    for (let cell = 0; cell < boardSize; cell++) {
+      const id = `${row}-${cell}`
+      grid[row][cell] = { isMine: false, row, cell }
+      HTML += `<div id="${id}" class="cell"></div>`
+    }
   }
   document.getElementById('game-container').innerHTML = HTML
 }
@@ -13,14 +18,55 @@ function createBoard() {
 createBoard();
 
 function addMines() {
-  const max = boardSize * boardSize
-  const amountOfMines = max * difficulty;
+  const amountOfMines = (boardSize * boardSize) * difficulty;
 
   for (let i = 0; i < amountOfMines; i++) {
-    let id = Math.floor(Math.random() * max)
-    document.getElementById(`cell-${id}`).classList.add('mine');
+    const randRow = Math.floor(Math.random() * boardSize)
+    let randCell = Math.floor(Math.random() * boardSize)
+    grid[randRow][randCell].isMine = true;
+    document.getElementById(`${randRow}-${randCell}`).classList.add('mine');
   }
 }
 
 addMines();
 
+const directions = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1], [0, 1],
+  [1, -1], [1, 0], [1, 1]
+]
+
+function countMines(row, cell) {
+  let mineCount = 0;
+
+  for (let i = 0; i < 8; i++) {
+    const newRow = row + directions[i][0];
+    const newCell = cell + directions[i][1];
+    
+    if (newRow >= 0 &&
+      newRow < boardSize &&
+      newCell >= 0 &&
+      newCell < boardSize &&
+      grid[newRow][newCell].isMine
+    ) {
+      mineCount++
+    }
+  }
+  return mineCount;
+}
+
+document.querySelectorAll('.cell').forEach((e) => {
+  e.addEventListener('click', (event) => {
+    const [row, cell] = event.target.id.split('-').map(Number);
+    const mineCount = countMines(row, cell);
+    if (mineCount === 0) {
+      for (let i = 0; i < 8; i++) {
+        const newRow = row + directions[i][0];
+        const newCell = cell + directions[i][1]; 
+        const newMines = countMines(newRow, newCell);
+        document.getElementById(`${newRow}-${newCell}`).textContent = newMines;
+      }
+    }
+    event.target.textContent = mineCount;
+  });
+});
